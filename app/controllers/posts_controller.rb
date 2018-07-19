6e10b03b1b0d9610394b5c9bd6ab1b9da7557aaf
Minkyung <mnk918p@gmail.com>
2018-07-19 14:19:24 +0900

    develop

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-  before_action :set_post, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, only: [ :new, :edit, :create, :update, :destroy ]
 
   # GET /posts
   # GET /posts.json
