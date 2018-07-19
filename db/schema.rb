25f1e25e52d445e6b4612889cf387e9ef0f89561
LDongGyu <ehdrbchd@naver.com>
2018-07-19 14:09:53 +0900

    faq 및 tutorial 추가

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,15 @@
 
 ActiveRecord::Schema.define(version: 20180718102206) do
 
+  create_table "comments", force: :cascade do |t|
+    t.string "content"
+    t.string "user"
+    t.integer "post_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_comments_on_post_id"
+  end
+
   create_table "posts", force: :cascade do |t|
     t.string "nickname"
     t.string "title"
