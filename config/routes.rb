d84bae9c3e8bb3941414697d7c5858fffc6ead25
LDongGyu <ehdrbchd@naver.com>
2018-07-22 22:29:04 +0900

    payment 추가

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'payment/index'
+
   devise_for :users
 
   root 'main#index'
@@ -8,5 +10,6 @@ Rails.application.routes.draw do
   get 'faqs/index' => 'faqs#index'
   get 'tutorials/index' => 'tutorials#index'
 
+  post 'payment/index' => 'payment#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
