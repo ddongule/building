53a777823f4ad8ce630ed777a75eca99e9ea2683
Minkyung <mnk918p@gmail.com>
2018-07-22 17:27:32 +0900

    main index

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
+  root 'main/index'
   devise_for :users
   
-  root 'faqs#index'
+
   get 'faqs/index' => 'faqs#index'
-  
   get 'tutorials/index' => 'tutorials#index' 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
