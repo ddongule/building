0174816254f1ef0c504043d21835cb1ced022de1
Minkyung <mnk918p@gmail.com>
2018-07-22 21:31:19 +0900

    css change

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,10 @@ Rails.application.routes.draw do
 
   root 'main#index'
   
-  get 'order/index'
-  get 'intro/index'
-  get 'faqs/index' => 'faqs#index'
-  get 'tutorials/index' => 'tutorials#index'
+  get 'order/index', :as => 'order'
+  get 'intro/index', :as => 'intro_index'
+  get 'faqs/index' => 'faqs#index', :as => 'faq'
+  get 'tutorials/index' => 'tutorials#index', :as => 'tutorials'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
