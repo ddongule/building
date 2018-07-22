a0349f12d09ead65250149e6cc5581c62ac2266a
Minkyung <mnk918p@gmail.com>
2018-07-22 17:23:01 +0900

    drop

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,10 @@
 Rails.application.routes.draw do
-
   devise_for :users
-  resources :posts
-
-  root 'posts#index'
   
+  root 'faqs#index'
   get 'faqs/index' => 'faqs#index'
   
   get 'tutorials/index' => 'tutorials#index' 
-    
-  get 'posts/show/:id' => 'posts#show'
-
-  post 'posts/show/:post_id/comments/create' => 'comments#create'
-
-  get 'posts/show/:post_id/comments/destory/:comment_id' => 'comments#destory'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
