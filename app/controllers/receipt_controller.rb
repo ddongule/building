e0025718f23b127a7b05f42770f8d36e2b7bb10d
LDongGyu <ehdrbchd@naver.com>
2018-07-29 21:26:40 +0900

    receipt 확인

--- app/controllers/receipt_controller.rb
+++ app/controllers/receipt_controller.rb
@@ -1,5 +1,11 @@
 class ReceiptController < ApplicationController
   def index
-    @test = params[:@value[0]]
+    @amount = Array.new
+    @wineName = params[:buyInfo]
+    @list = @wineName.split(",")
+    for i in 0..@list.length
+      @test = params[@list[i]]
+      @amount.push(@test)
+    end
   end
 end
