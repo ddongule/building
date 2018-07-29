33fe3a869d51605fe600a4ad08b34e439d17f773
Minkyung <mnk918p@gmail.com>
2018-07-29 21:31:47 +0900

    수정사항 변경

--- app/controllers/receipt_controller.rb
+++ app/controllers/receipt_controller.rb
@@ -1,8 +1,5 @@
 class ReceiptController < ApplicationController
   def index
-<<<<<<< HEAD
-    @test = params
-=======
     @amount = Array.new
     @wineName = params[:buyInfo]
     @list = @wineName.split(",")
@@ -10,6 +7,5 @@ class ReceiptController < ApplicationController
       @test = params[@list[i]]
       @amount.push(@test)
     end
->>>>>>> 5b75f2eadeeb9f7441fe1d9212fbaf9824c09fd1
   end
 end
